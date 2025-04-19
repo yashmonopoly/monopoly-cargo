a81bf5ef1383ac6fb7a90ba49f9c013e0e5bbb29
yashmonopoly <yashmonopoly@gmail.com>
2025-04-19 17:00:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPanel from './LoginPanel';
+import DashboardPanel from './DashboardPanel';
+import CdBookingPanel from './CdBookingPanel';
+import ManifestPanel from './ManifestPanel';
+import AwbPanel from './AwbPanel';
+import AccountsPanel from './AccountsPanel';
+import ReportsMISPanel from './ReportsMISPanel';
+import StockPanel from './StockPanel';
+import AdminPanel from './AdminPanel';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPanel />} />
+        <Route path="/dashboard" element={<DashboardPanel />} />
+        <Route path="/cd-booking" element={<CdBookingPanel />} />
+        <Route path="/manifest" element={<ManifestPanel />} />
+        <Route path="/awb" element={<AwbPanel />} />
+        <Route path="/accounts" element={<AccountsPanel />} />
+        <Route path="/reports" element={<ReportsMISPanel />} />
+        <Route path="/stock" element={<StockPanel />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
